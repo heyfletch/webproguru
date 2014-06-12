45ccc398c0fdd4e4d246a9b467531a5e767fae35
Joe Fletcher <Joe@merchantguru.com>
2014-06-12 10:16:53 -0700

    some basic styling

--- wp-content/themes/merchantguru/lib/scripts.php
+++ wp-content/themes/merchantguru/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '9880649384aea9f1ee166331c0a30daa');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'ed59f7d84736f622811b89ad427f0b60');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
