89f1bf1794a5505743ce102fabe97bc446f4ce96
Joe Fletcher <Joe@merchantguru.com>
2014-06-20 13:06:06 -0700

    install roots sass theme

--- wp-content/themes/merchantguru/lib/scripts.php
+++ wp-content/themes/merchantguru/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'ed59f7d84736f622811b89ad427f0b60');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '64c2848549e90cef42796141ccce4c3e');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
